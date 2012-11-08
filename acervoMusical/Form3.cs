7ac95f6050e4096227d200d2f722b3e56efaa150
andrei <andrei.ifsp@hotmail.com>
2012-11-08 19:40:52 -0200

    Envio de todos os forms

---
+++ acervoMusical/Form3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace acervoMusical
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == "digital")
+            {
+                label2.Visible = true;
+                textBox2.Visible = true;
+            }
+            else
+            {
+                label2.Visible = false;
+                textBox2.Visible = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
