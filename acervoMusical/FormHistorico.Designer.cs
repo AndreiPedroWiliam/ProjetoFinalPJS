ccf215422003c03a473a53bbc0a64d76fcc35d4e
andrei <andrei.ifsp@hotmail.com>
2012-12-11 21:13:11 -0200

    pequenos ajustes

--- acervoMusical/FormHistorico.Designer.cs
+++ acervoMusical/FormHistorico.Designer.cs
@@ -81,11 +81,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(779, 485);
+            this.button1.Location = new System.Drawing.Point(737, 485);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(117, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Salvar";
+            this.button1.Text = "Salvar em arquivo...";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
